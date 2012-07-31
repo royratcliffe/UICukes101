874a7683f943a50b721b130a67e89cf3485cc51c
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2012-07-31 10:21:06 +0100

    Steps belong to tests, not the project top-level

--- UICukes101Tests/PersonalGreetingSteps.m
+++ UICukes101Tests/PersonalGreetingSteps.m
@@ -1,6 +1,6 @@
 //
 //  PersonalGreetingSteps.m
-//  UICukes101
+//  UICukes101Tests
 //
 //  Created by Roy Ratcliffe on 30/07/2012.
 //  Copyright (c) 2012 The OCCukes Organisation. All rights reserved.
